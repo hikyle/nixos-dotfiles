2707f5c57182c38248a581baf68f2fe708bc311d
kyle <elyktun@gmail.com>
2025-09-08 23:12:08 -0400

    202509081112

--- configuration.nix
+++ configuration.nix
@@ -1,89 +1,91 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
+	imports = [ ./hardware-configuration.nix ];
 
-  hardware.graphics.enable = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.blacklistedKernelModules = [ "iwlwifi" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
-  time.timeZone = "America/New_York";
-  nixpkgs.config.allowUnfree = true;
+	hardware.graphics.enable = true;
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	boot.blacklistedKernelModules = [ "iwlwifi" ];
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+	networking.hostName = "nixos";
+	networking.networkmanager.enable = true;
+	time.timeZone = "America/New_York";
+	nixpkgs.config.allowUnfree = true;
 
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "nvidia" ];
-  };
+	services.xserver = {
+		enable = true;
+		videoDrivers = [ "nvidia" ];
+	};
 
-  services.displayManager.sddm.enable = true;
-  services.flatpak.enable = true;
-  services.pulseaudio.enable = false;
-  services.power-profiles-daemon.enable = true;
-  security.rtkit.enable = true;
+	services.displayManager.sddm.enable = true;
+	services.flatpak.enable = true;
+	services.pulseaudio.enable = false;
+	services.power-profiles-daemon.enable = true;
+	security.rtkit.enable = true;
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
+	hardware.nvidia = {
+		modesetting.enable = true;
+		open = true;
+		nvidiaSettings = true;
+		package = config.boot.kernelPackages.nvidiaPackages.beta;
+	};
 
-  users.users.kyle = {
-    isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [ ];
-  };
+	users.users.kyle = {
+		isNormalUser = true;
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; [ ];
+	};
 
-  programs.firefox.enable = true;
-  programs.steam.enable = true;
-  programs.waybar.enable = true;
-  programs.hyprland.enable = true;
+	programs.firefox.enable = true;
+	programs.steam.enable = true;
+	programs.waybar.enable = true;
+	programs.hyprland.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-    gnumake
-    unzip
-    gcc
-    ripgrep
-    fd
-    eza
-    bat
-    neovim
-    protonplus
-    fastfetch
-    heroic
-    gh
-    wl-clipboard
-    papirus-icon-theme
-    kitty
-    nwg-look
-    waypaper
-    hyprpaper
-    xdg-desktop-portal-gtk
-    mate.mate-polkit
-    pulseaudioFull
-    pavucontrol
-    hyprshot
-    grim
-    hyprpicker
-    inter-nerdfont
-  ];
+	environment.systemPackages = with pkgs; [
+		wget
+		git
+		gnumake
+		unzip
+		gcc
+		ripgrep
+		fd
+		eza
+		bat
+		neovim
+		protonplus
+		fastfetch
+		heroic
+		gh
+		wl-clipboard
+		papirus-icon-theme
+		kitty
+		nwg-look
+		waypaper
+		hyprpaper
+		xdg-desktop-portal-gtk
+		mate.mate-polkit
+		pulseaudioFull
+		pavucontrol
+		hyprshot
+		grim
+		hyprpicker
+		fuzzel
+		bibata-cursors
+	];
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
+	fonts.packages = with pkgs; [
+		nerd-fonts.jetbrains-mono
+		nerd-fonts.ubuntu
+	];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "25.05";
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	system.stateVersion = "25.05";
 }
